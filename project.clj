d980ed7c5148a72335569785a0864c2fb3b8b615
Eric Richmond <eric.richmond@gmail.com>
2013-12-16 17:49:48 -0500

    Update dependencies and bump version number

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject jopbox "0.2.0"
+(defproject jopbox "0.2.2"
   :description "A Clojure Library to work with Dropbox API"
   :url "http://github.com/samrat/jopbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-oauth "1.4.0"]
-                 [cheshire "5.0.1"]])
+                 [clj-oauth "1.4.1"]
+                 [cheshire "5.2.0"]])
